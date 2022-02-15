0868e016851a27e75beedbbf9d424704e86a461d
Sidi BA <sidi1.ba@epitech.eu>
2022-02-15 02:07:43 +0100

    [UPDATE] strchr

--- src/strchr.asm
+++ src/strchr.asm
@@ -8,21 +8,21 @@ strchr:
 
 loop_on_str:
         cmp byte [rdi], 0  ; test end of string
-        je fail_end     ; go to end
+        je no_found     ; go to end
 
         cmp byte [rdi], sil ; check if current character is equal to character given in parameter
-        je strchr_end   ; go to end
+        je found_end   ; go to end
 
         inc rdi         ; move to next character of the string
         jmp loop_on_str         ; loop
 
-strchr_end:
+found_end:
         mov rax, rdi            ; set function return
         mov rsp, rbp
         pop rbp
         ret
 
-fail_end:
+no_found:
         mov rax, 0
         mov rsp, rbp
         pop rbp
