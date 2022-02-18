fc727607d94ec84e0050324b94b21134e2176ef5
Sidi BA <sidi1.ba@epitech.eu>
2022-02-18 23:59:35 +0100

    [FIX] memcpy

--- src/memcpy.asm
+++ src/memcpy.asm
@@ -2,25 +2,23 @@ BITS 64
 section .text
     GLOBAL memcpy
 
-; void *memcpy(void *dest, const void *src, size_t n);
+;   void *memcpy(void *dest, const void *src, size_t n);
 
 memcpy:
     push rbp
     mov rbp, rsp
-    mov r8, rdi         ; store adress rdi in r8
-
-loop_cpy:
-    cmp rdx, 0
-    je return_dest
-    mov r9b, byte [rsi] ; copy byte src into dest
-    mov byte [rdi], r9b ; 
+    mov rax, rdi
     dec rdx
-    inc rsi
-    inc rdi
-    jmp loop_cpy
+
+loop:
+    cmp rdx, 0
+    jl return_dest
+    mov r8b, byte [rsi + rdx] ; copy byte src into dest
+    mov byte [rdi + rdx], r8b; 
+    dec rdx
+    jmp loop
 
 return_dest:
-    mov rax, r8     ; set return to adress r8
     mov rsp, rbp
     pop rbp
     ret
--- src/memset.asm
+++ src/memset.asm
@@ -2,12 +2,12 @@ BITS 64
 section .text
     GLOBAL memset
 
-; void *memset(void *s, int c, size_t n);
+;   void *memset(void *s, int c, size_t n);
 
 memset:
     push rbp
     mov rbp, rsp
-    mov r8, rdi         ; store adress rdi in r8
+    mov rax, rdi         ; store adress rdi in r8
 
 loop:
     cmp rdx, 0
@@ -18,7 +18,6 @@ loop:
     jmp loop
 
 return_dest:
-    mov rax, r8     ; set return to adress r8
     mov rsp, rbp
     pop rbp
     ret
